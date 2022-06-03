462080c6c2b6d6626ab743d3d64d3656566bb178
dejavu284 <skuratov.1@mail.ru>
2022-06-03 17:36:33 +0700

    First version of my OpenFileEnglishLearn programm. Just TEST

---
+++ OpenFileEnglishLearn/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace OpenFileEnglishLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Them: Databases\n\n");
+                string FilePath = "C:/Users/Я/Desktop/текст по английскому учить.txt";
+                StreamReader streamReader = new StreamReader(FilePath);
+                string AllFile = streamReader.ReadToEnd();
+                string[] EachLine = AllFile.Split("..");
+                int IndexLine = 0;
+                while (IndexLine < EachLine.Length-1)
+                {
+                    Console.WriteLine(EachLine[IndexLine]);
+                    Console.WriteLine("\n{0} - Строк осталось", EachLine.Length - IndexLine - 2);
+                    Console.ReadKey(true);
+                    IndexLine++;
+                }
+                Console.WriteLine("\n\n\n\n");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
